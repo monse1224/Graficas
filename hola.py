ea264b5f8871f540918fbb4434e19958d70985ba
monse1224 <A01709064@tec.mx>
2024-04-22 18:30:41 -0600

    cambios

--- hola.py
+++ hola.py
@@ -17,15 +17,16 @@ ax.hist(titanic_data['Fare'])
 st.header("Histograma del Titanic")
 st.pyplot(fig)
 
-fig2,ax2 =plt.subplots()
-y_pos=titanic_data['Pclass']
-x_pos=titanic_data['Fare']
-ax2=barh(y_pos,x_pos)
-ax2.set_ylabel('Class')
-ax2.set_laxbel('Fare')
-ax2.set_title('¿Cuánto pagaron las clases del titanic?')
-st.header('Gráfica de barras del titanic')
+fig2, ax2 = plt.subplots()
+y_pos = titanic_data['Pclass']
+x_pos = titanic_data['Fare']
+ax2.barh(y_pos, x_pos)
+ax2.set_ylabel("Pclass")
+ax2.set_xlabel("Fare")
+ax2.set_title('¿Cuanto pagaron las clases del Titanic')
+st.header("Grafica de Barras del Titanic")
 st.pyplot(fig2)
+
 
 fig3,ax3=plt.subplots()
 ax3.scatter(titanic_data['Age'],titanic_data['Fare'])
@@ -39,6 +40,28 @@ ax4=titanic_data.boxplot(['Age'])
 ax4.set_ylabel('Edad')
 st.header('Gráfica de cajas por edad')
 st.pyplot(fig4)
+
+import numpy as np
+
+fig5, ax5 = plt.subplots()
+hist_class = np.histogram(titanic_data["Pclass"], bins=3, range=(1,3))[0]
+
+
+labels = ['1', '2', '3']
+colors = ['tab:blue', 'tab:red', 'tab:green']
+explode = [0, 0, 0.2]
+
+
+ax5.pie(hist_class,
+       labels = labels,
+         colors = colors,
+         autopct='%.0f%%',
+         explode = explode,
+         shadow = True)
+st.header("Grafica de pastel - Clase social")
+st.pyplot(fig5)
+st.dataframe(hist_class)
+
 
 
 
